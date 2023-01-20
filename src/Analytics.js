fb0222bc54113981a101ac3d9f9b2b265e1dd313
Suketh K Srivatsan <122083441+Sukethksrivatsan@users.noreply.github.com>
2023-01-20 13:05:15 +0530

    Add files via upload

---
+++ src/Analytics.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DisplayTable from './components/DisplayTable';
+import './Analytics.css';
+const App = () => {
+  return <div className='display'>
+    <h1>GreedyGame Frontend Assessment</h1>
+    <h2>Select dates within a specific range of 1-Jun-2021 to
+31-Jun-2021</h2>
+  <pre id="defaultperiod">By default a period between 1-Jun and 30-Jun has been selected</pre>
+    <pre className='tableheading'>Table containing values as mentioned in the Assignment</pre>
+    <DisplayTable/>
+  </div>
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Analytics from './Analytics';
+
+ReactDOM.render(
+<Analytics/>,
+ document.getElementById('root')
+ );
